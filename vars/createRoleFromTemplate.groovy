07df2a53ea60d5231e4b29e60176b1ba001cc58a
DeepDhiman03 <118919161+DeepDhiman03@users.noreply.github.com>
2025-09-06 16:38:07 +0530

    Update createRoleFromTemplate.groovy

--- vars/createRoleFromTemplate.groovy
+++ vars/createRoleFromTemplate.groovy
@@ -1,4 +1,4 @@
 // vars/createRoleFromTemplate.groovy
-def call(String roleName, String pattern, String templateRoleName) {
+def call(String roleName, String pattern, String templateRoleName = "Read") {
     com.yourorg.rbac.RoleUtils.createRoleFromTemplate(roleName, pattern, templateRoleName)
 }
